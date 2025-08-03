a5b266571b4adb37a17c232dced22e73499a9f47
we sell insurance <neelesh.poli2006@gmail.com>
2025-08-03 18:25:04 -0500

    Use TabBar for navigation - Tabbar wlll display at the top of the screen on screen width over 700 pixels - Tabbar will display at the bottom of the screen for screen width under 700 pixels

--- lib/pages/navigation.dart
+++ lib/pages/navigation.dart
@@ -3,44 +3,34 @@ import 'package:rr_app/pages/featured.dart';
 import 'package:rr_app/pages/menu.dart';
 import 'package:vector_graphics/vector_graphics_compat.dart';
 
-class Navigation extends StatefulWidget {
-  const Navigation({super.key});
-
-  @override
-  State<StatefulWidget> createState() => NavigationState();
-}
-
-class NavigationState extends State<Navigation> {
+class Navigation extends StatelessWidget {
   static const Widget logo = VectorGraphic(
     loader: AssetBytesLoader('assets/icons/icon.svg'),
   );
 
-  int pageIndex = 0;
+  const Navigation({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: logo,
-        centerTitle: true,
+    const tabBar = TabBar(
+      tabs: [
+        Tab(text: 'Featured', icon: Icon(Icons.star_rounded)),
+        Tab(text: 'Menu', icon: Icon(Icons.menu_book_rounded)),
+      ],
+    );
+
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: logo,
+          centerTitle: true,
+          bottom: MediaQuery.of(context).size.width >= 700 ? tabBar : null,
+        ),
+        bottomNavigationBar: MediaQuery.of(context).size.width < 700 ? tabBar : null,
+        body: const TabBarView(children: [FeaturedPage(), MenuPage()]),
       ),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: pageIndex,
-        onDestinationSelected: (int index) {
-          setState(() {
-            pageIndex = index;
-          });
-        },
-        destinations: const <Widget>[
-          NavigationDestination(
-            icon: Icon(Icons.star_outline_rounded),
-            selectedIcon: Icon(Icons.star_rounded),
-            label: 'Featured',
-          ),
-          NavigationDestination(icon: Icon(Icons.menu), label: 'Menu'),
-        ],
-      ),
-      body: const <Widget>[FeaturedPage(), MenuPage()][pageIndex],
     );
   }
 }
