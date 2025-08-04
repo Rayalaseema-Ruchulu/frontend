a6963d05ca88bde8fb8e94c1f93db55393d726aa
we sell insurance <neelesh.poli2006@gmail.com>
2025-08-04 07:59:24 -0500

    Adjust navigation mode based on availible space - Use NavigationRail for larger spaces - Use NavigationBar with AppBar for smaller spaces

--- lib/pages/navigation.dart
+++ lib/pages/navigation.dart
@@ -3,34 +3,82 @@ import 'package:rr_app/pages/featured.dart';
 import 'package:rr_app/pages/menu.dart';
 import 'package:vector_graphics/vector_graphics_compat.dart';
 
-class Navigation extends StatelessWidget {
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<StatefulWidget> createState() => NavigationState();
+}
+
+class NavigationState extends State<Navigation> {
   static const Widget logo = VectorGraphic(
     loader: AssetBytesLoader('assets/icons/icon.svg'),
   );
 
-  const Navigation({super.key});
+  int _pageIndex = 0;
+  final _pages = const <Widget>[FeaturedPage(), MenuPage()];
 
   @override
   Widget build(BuildContext context) {
-    const tabBar = TabBar(
-      tabs: [
-        Tab(text: 'Featured', icon: Icon(Icons.star_rounded)),
-        Tab(text: 'Menu', icon: Icon(Icons.menu_book_rounded)),
-      ],
-    );
-
-    return DefaultTabController(
-      initialIndex: 0,
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: logo,
-          centerTitle: true,
-          bottom: MediaQuery.of(context).size.width >= 700 ? tabBar : null,
-        ),
-        bottomNavigationBar: MediaQuery.of(context).size.width < 700 ? tabBar : null,
-        body: const TabBarView(children: [FeaturedPage(), MenuPage()]),
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth >= 700) {
+          return Scaffold(
+            body: SafeArea(
+              child: Row(
+                children: [
+                  NavigationRail(
+                    leading: logo,
+                    destinations: [
+                      const NavigationRailDestination(
+                        icon: Icon(Icons.star_outline_rounded),
+                        selectedIcon: Icon(Icons.star_rounded),
+                        label: Text('Featured'),
+                      ),
+                      const NavigationRailDestination(
+                        icon: Icon(Icons.menu_book_rounded),
+                        label: Text('Menu'),
+                      ),
+                    ],
+                    onDestinationSelected: (value) {
+                      setState(() {
+                        _pageIndex = value;
+                      });
+                    },
+                    selectedIndex: _pageIndex,
+                    labelType: NavigationRailLabelType.all,
+                  ),
+                  const VerticalDivider(width: 1, thickness: 1),
+                  Expanded(
+                    child: IndexedStack(index: _pageIndex, children: _pages),
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(title: logo, centerTitle: true),
+            bottomNavigationBar: NavigationBar(
+              selectedIndex: _pageIndex,
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _pageIndex = index;
+                });
+              },
+              destinations: const <Widget>[
+                NavigationDestination(
+                  icon: Icon(Icons.star_outline_rounded),
+                  selectedIcon: Icon(Icons.star_rounded),
+                  label: 'Featured',
+                ),
+                NavigationDestination(icon: Icon(Icons.menu), label: 'Menu'),
+              ],
+            ),
+            body: IndexedStack(index: _pageIndex, children: _pages),
+          );
+        }
+      },
     );
   }
 }
